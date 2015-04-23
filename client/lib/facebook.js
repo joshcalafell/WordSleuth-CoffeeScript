edeccda33fbde483ab51c282e46aef86f4db07be
rabbitfighter81 <rabbitfighter@cryptolab.net>
2015-04-23 11:45:06 -0600

    Added FB and Twitter functionailty

--- client/lib/facebook.js
+++ client/lib/facebook.js
@@ -1,7 +1,13 @@
-Meteor.setInterval((function(d, s, id) {
+Template.header.rendered = function() {
+    try {
+        FB.XFBML.parse();
+    }catch(e) {}   
+};
+
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=591499857628792";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk')), 2000);
+}(document, 'script', 'facebook-jssdk'));
